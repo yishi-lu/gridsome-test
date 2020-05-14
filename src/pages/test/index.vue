7cebf9033d54ede252afa6810da5aeff320e9082
dante1218 <ttkkyyz18@gmail.com>
2020-05-14 18:28:43 -0400

    inital

---
+++ src/pages/test/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <Layout>
+
+    <g-link to="/about/">About us</g-link>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Hello, world!'
+  }
+}
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
